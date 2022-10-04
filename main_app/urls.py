c5d51d3985d996849cb7dcb926dd74200d0fd3a6
anthowest <anthowest14@gmail.com>
2022-10-04 12:40:53 -0500

    Intial commit, dependencies, views

---
+++ main_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+# this like app.use() in express
+urlpatterns = [
+    path('', views.Home.as_view(), name="home"),
+    path('about/', views.About.as_view(), name="about"),
+]
---
+++ main_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+
+# Create your views here.
+
+class Home(TemplateView):
+    template_name = "home.html"
+
+class About(TemplateView):
+    template_name = "about.html"
